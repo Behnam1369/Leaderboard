664578751642f5da64acadb58b97fa116e314955
Behnam Aghaali <Behnam.Aghaali@yahoo.com>
2022-03-02 00:19:46 +0330

    Setup webpack and create the wireframe

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+import './modules/showScores.js';
+
+// Game with ID: ffLA8bI6eF9yyXwbNJ4E added.
---
+++ src/modules/getData.js
@@ -0,0 +1,7 @@
+const scores = [
+  { user: 'Behnam', score: '99.5' },
+  { user: 'US2', score: '95' },
+  { user: 'ENEMY', score: '88' },
+];
+
+module.exports = scores;
